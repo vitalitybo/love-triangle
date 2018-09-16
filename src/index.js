623fa319aaa2dc8d8a55a1a2c065c17765359693
vitalitybo <v2.borisevich@gmail.com>
2018-09-16 19:49:20 +0300

    love-triangle 100 passing

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+
+  for (let i = 0; i < preferences.length; i++) {
+    let firstSp = preferences[i];
+    let secondSp = preferences[firstSp - 1];
+    let thirdSp = preferences[secondSp - 1];
+
+    if ((thirdSp === i + 1)) {
+
+      if (firstSp == secondSp || secondSp == thirdSp) continue;
+      
+      count++;
+      preferences[i] = 0;
+    };
+  };
+  return count;
 };
